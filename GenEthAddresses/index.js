8bcc609ba5822416e77000c4c5e5dd868e34d67f
Antoine De Vuyst <fortyonepointsix@gmail.com>
2020-06-30 16:52:13 -0400

    gen Ethereum Addresses

---
+++ GenEthAddresses/index.js
@@ -0,0 +1,40 @@
+
+
+
+let data
+
+
+
+   function getCSVData(){
+     data = new Array()
+     let  mnemonic = document.getElementById("seed").value
+     let provider = ethers.getDefaultProvider("homestead");
+
+     let wallet = ethers.Wallet.fromMnemonic(mnemonic)
+
+     let walletWithProvider = new ethers.Wallet(wallet.privateKey, provider);
+
+     let masterNode = ethers.utils.HDNode.fromMnemonic(mnemonic);
+
+     for (i=0;i<999;i++){
+       let address = masterNode.derivePath("m/44'/60'/0'/0/" + i);
+       let datapoint = new Object()
+       datapoint.address = address.address
+       data.push(datapoint)     }
+   }
+
+   function download_csv() {
+     getCSVData()
+       var csv = 'Address,Addresses\n';
+       data.forEach(function(datapoint) {
+               csv += datapoint.address;
+               csv += ",";
+               csv += "\n";
+       });
+
+       var hiddenElement = document.createElement('a');
+       hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+       hiddenElement.target = '_blank';
+       hiddenElement.download = 'Addresses.csv';
+       hiddenElement.click();
+   }
